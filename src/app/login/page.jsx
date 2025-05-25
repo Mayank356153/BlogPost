f757030a38d42adc77983556325b28b8c2095a9a
Mayank <mayanksharma07123@gmail.com>
2025-05-26 01:04:52 +0530

    download necessary dependencies

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Sidebar from '@/components/Sidebar'
+export default function page() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React,{useState} from 'react'
+import Link from 'next/link'
+export default function page() {
+  
+  const[mayank,setMayank]=useState(0)
+  return (
+    <div className='flex justify-center items-center bg-gray-400 w-full h-screen text-2xl font-bold'>
+    <Link href="/login">
+    
+     <h1>Login</h1>
+    </Link>
+    </div>
+  )
+}
